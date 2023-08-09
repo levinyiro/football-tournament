230488d5e45b19577c29cb6410bf32f42fa64e73
leventenyiro <nyiro.levente@gmail.com>
2023-08-09 15:58:52 +0200

    #12: start modifying group data

--- tournament-web/src/data/Data.js
+++ tournament-web/src/data/Data.js
@@ -25,6 +25,72 @@ class Data {
         return jsonData.find(tournament => tournament.id === id);
     }
 
+    static async getGroups(id) {
+        const tournament = jsonData.find(tournament => tournament.id === id)
+
+
+        tournament.groups.map(group => {
+            const players = tournament.players.map(playerId => {
+                const player = tournament.players.find(p => p.id === playerId);
+                console.log(player);
+                const gamesPlayed = group.matches.filter(
+                    match => match.playerAId === playerId || match.playerBId === playerId
+                ).length;
+                const won = group.matches.filter(
+                    match =>
+                        (match.playerAId === playerId && match.scoreA > match.scoreB) ||
+                        (match.playerBId === playerId && match.scoreB > match.scoreA)
+                ).length;
+                const drawn = group.matches.filter(
+                    match =>
+                        match.scoreA === match.scoreB &&
+                        (match.playerAId === playerId || match.playerBId === playerId)
+                ).length;
+                const lost = gamesPlayed - won - drawn;
+
+                const gf = group.matches
+                    .filter(match => match.playerAId === playerId)
+                    .map(match => match.scoreA)
+                    .reduce((a, c) => a + c, 0) + group.matches
+                        .filter(match => match.playerBId === playerId)
+                        .map(match => match.scoreB)
+                        .reduce((a, c) => a + c, 0);
+
+                const ga = group.matches
+                    .filter(match => match.playerAId === playerId)
+                    .map(match => match.scoreB)
+                    .reduce((a, c) => a + c, 0) + group.matches
+                        .filter(match => match.playerBId === playerId)
+                        .map(match => match.scoreA)
+                        .reduce((a, c) => a + c, 0);
+
+                const gd = gf - ga;
+                const points = won * 3 + drawn;
+
+
+                return {
+                    id: playerId,
+                    name: player.name,
+                    team: player.team,
+                    matchPlayed: gamesPlayed,
+                    won: won,
+                    draw: drawn,
+                    lose: lost,
+                    gf: gf,
+                    ga: ga,
+                    gd: gd,
+                    points: points
+                };
+            });
+
+            return {
+                name: group.name,
+                players: players
+            };
+        });
+
+    }
+
     // static updateTournaments(modifiedData) {
     //     this.tournaments = modifiedData;
     // }
--- tournament-web/src/pages/tournament/Tournament.js
+++ tournament-web/src/pages/tournament/Tournament.js
@@ -8,6 +8,7 @@ function Tournament() {
   const { id } = useParams();
   const [actualTab, setActualTab] = useState('group');
   const [tournament, setTournament] = useState(null);
+  const [groups, setGroups] = useState(null);
   const { isLoggedIn, setIsLoggedIn } = useAuth();
 
   useEffect(() => {
@@ -20,6 +21,9 @@ function Tournament() {
 
     if (data.groups == null)
       setActualTab('knockout');
+    else {
+      const groups = await Data.getGroups(id);
+    }
   };
 
   return (
@@ -57,10 +61,13 @@ function Tournament() {
                         <th>Pos</th>
                         <th>Name</th>
                         <th>Team</th>
-                        <th>Games Played</th>
-                        <th>Won</th>
-                        <th>Draw</th>
-                        <th>Lose</th>
+                        <th>MP</th>
+                        <th>W</th>
+                        <th>D</th>
+                        <th>L</th>
+                        <th>GF</th>
+                        <th>GA</th>
+                        <th>GD</th>
                         <th>Points</th>
                       </tr>
                     </thead>
@@ -78,12 +85,32 @@ function Tournament() {
                             (match.playerAId === playerId && match.scoreA > match.scoreB) ||
                             (match.playerBId === playerId && match.scoreB > match.scoreA)
                         ).length;
+
                         const drawn = group.matches.filter(
                           match =>
                             match.scoreA === match.scoreB &&
                             (match.playerAId === playerId || match.playerBId === playerId)
                         ).length;
+
                         const lost = gamesPlayed - won - drawn;
+
+                        const gf = group.matches
+                          .filter(match => match.playerAId === playerId)
+                          .map(match => match.scoreA)
+                          .reduce((a, c) => a + c, 0) + group.matches
+                          .filter(match => match.playerBId === playerId)
+                          .map(match => match.scoreB)
+                          .reduce((a, c) => a + c, 0);
+
+                        const ga = group.matches
+                          .filter(match => match.playerAId === playerId)
+                          .map(match => match.scoreB)
+                          .reduce((a, c) => a + c, 0) + group.matches
+                          .filter(match => match.playerBId === playerId)
+                          .map(match => match.scoreA)
+                          .reduce((a, c) => a + c, 0);
+
+                        const gd = gf - ga;
 
                         return (
                           <tr key={playerIndex}>
@@ -94,6 +121,9 @@ function Tournament() {
                             <td>{won}</td>
                             <td>{drawn}</td>
                             <td>{lost}</td>
+                            <td>{gf}</td>
+                            <td>{ga}</td>
+                            <td>{gd}</td>
                             <td style={{ fontWeight: 'bold' }}>{won * 3 + drawn}</td>
                           </tr>
                         );
@@ -160,12 +190,18 @@ function Tournament() {
               <h3 className='text-center mt-5 lead'>Play-off</h3>
               <div className="container card p-3 mt-4 gold">
                 <div className='row d-flex align-items-center'>
-                  <div className='col-4 text-center team winner'>
-                    <h4>Levi</h4>
-                    <p>Manchester City</p>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-trophy" viewBox="0 0 16 16">
-                      <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935zM3.504 1c.007.517.026 1.006.056 1.469.13 2.028.457 3.546.87 4.667C5.294 9.48 6.484 10 7 10a.5.5 0 0 1 .5.5v2.61a1 1 0 0 1-.757.97l-1.426.356a.5.5 0 0 0-.179.085L4.5 15h7l-.638-.479a.501.501 0 0 0-.18-.085l-1.425-.356a1 1 0 0 1-.757-.97V10.5A.5.5 0 0 1 9 10c.516 0 1.706-.52 2.57-2.864.413-1.12.74-2.64.87-4.667.03-.463.049-.952.056-1.469H3.504z" />
-                    </svg>
+                  <div className='col-4 text-center team winner row'>
+                    <div className='col-2 mt-3'>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-trophy" viewBox="0 0 16 16">
+                        <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935zM3.504 1c.007.517.026 1.006.056 1.469.13 2.028.457 3.546.87 4.667C5.294 9.48 6.484 10 7 10a.5.5 0 0 1 .5.5v2.61a1 1 0 0 1-.757.97l-1.426.356a.5.5 0 0 0-.179.085L4.5 15h7l-.638-.479a.501.501 0 0 0-.18-.085l-1.425-.356a1 1 0 0 1-.757-.97V10.5A.5.5 0 0 1 9 10c.516 0 1.706-.52 2.57-2.864.413-1.12.74-2.64.87-4.667.03-.463.049-.952.056-1.469H3.504z" />
+                      </svg>
+                    </div>
+                    <div className='col-10'>
+                      <div>
+                        <h4>Levi</h4>
+                        <p>Manchester City</p>
+                      </div>
+                    </div>
                   </div>
                   <div className='col-1'></div>
                   <div className='col-2 result py-2 text-center'>2 - 1</div>
