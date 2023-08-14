fac914f480cc9ab77066451be66e08f1d1f9febf
Levente Nyírő <nyiro.levente@gmail.com>
2023-08-14 13:20:24 +0200

    #12: frontend data (#18)
    * #12: add new tournament
    
    * #12: modify group
    
    * #12: start modifying group data
    
    * #12: ready with groups and add null case
    
    * #12: group ready visualization
    
    * #12: ready with matches
    
    * #12: modify matches tab
    
    * #12: start to implement knockout
    
    * #12: modify frontend data and ready with knockout and matches display
    
    * #12: modify json name

--- tournament-web/src/data/Data.js
+++ tournament-web/src/data/Data.js
@@ -1,4 +1,4 @@
-import jsonData from './tournaments2.json';
+import jsonData from './tournaments.json';
 
 class Data {
     // static async getTournaments() {
@@ -23,6 +23,157 @@ class Data {
 
     static async getTournament(id) {
         return jsonData.find(tournament => tournament.id === id);
+    }
+
+    static async getGroups(id) {
+        const tournament = jsonData.find(tournament => tournament.id === id);
+
+        const data = tournament.groups.map(group => {
+            const players = group.players.map(playerId => {
+                const playerDetails = tournament.players.find(player => player.id === playerId);
+
+                const gamesPlayed = group.matches.filter(
+                    match => (match.playerAId === playerId || match.playerBId === playerId) &&
+                        match.scoreA !== null && match.scoreB !== null
+                ).length;
+
+                const won = group.matches.filter(
+                    match =>
+                        (match.playerAId === playerId && match.scoreA > match.scoreB) ||
+                        (match.playerBId === playerId && match.scoreB > match.scoreA)
+                ).length;
+
+                const drawn = group.matches.filter(
+                    match =>
+                        match.scoreA === match.scoreB &&
+                        (match.playerAId === playerId || match.playerBId === playerId) &&
+                        match.scoreA !== null && match.scoreB !== null
+                ).length;
+
+                const gf = group.matches
+                    .filter(match => match.playerAId === playerId)
+                    .map(match => match.scoreA)
+                    .reduce((a, c) => a + c, 0) + group.matches
+                        .filter(match => match.playerBId === playerId)
+                        .map(match => match.scoreB)
+                        .reduce((a, c) => a + c, 0);
+
+                const ga = group.matches
+                    .filter(match => match.playerAId === playerId)
+                    .map(match => match.scoreB)
+                    .reduce((a, c) => a + c, 0) + group.matches
+                        .filter(match => match.playerBId === playerId)
+                        .map(match => match.scoreA)
+                        .reduce((a, c) => a + c, 0);
+
+                return {
+                    id: playerId,
+                    name: playerDetails.name,
+                    team: playerDetails.team,
+                    matchPlayed: gamesPlayed,
+                    won: won,
+                    draw: drawn,
+                    lose: gamesPlayed - won - drawn,
+                    gf: gf,
+                    ga: ga,
+                    gd: gf - ga,
+                    points: won * 3 + drawn
+                };
+            });
+
+            players.sort((a, b) => {
+                if (a.points !== b.points) {
+                    return b.points - a.points;
+                } else if (a.gd !== b.gd) {
+                    return b.gd - a.gd;
+                } else {
+                    return b.gf - a.gf;
+                }
+            });
+
+            return {
+                name: group.name,
+                players: players,
+                isReady: players.every(player => player.matchPlayed === players.length - 1)
+            };
+        });
+
+        return data;
+    }
+
+    static async getMatches(id) {
+        const tournament = jsonData.find(tournament => tournament.id === id);
+        const allMatches = [];
+
+        if (tournament.groups) {
+            allMatches.push(...tournament.groups.map(group => {
+                const matches = group.matches.map(match => {
+                    const playerA = tournament.players.find(player => player.id === match.playerAId);
+                    const playerB = tournament.players.find(player => player.id === match.playerBId);
+                    const winner = match.scoreA > match.scoreB 
+                        ? match.playerAId 
+                        : (match.scoreB > match.scoreA ? match.playerBId : null);
+        
+                    return {
+                        ...match,
+                        winner,
+                        playerA,
+                        playerB
+                    };
+                })
+                
+                return {
+                    name: group.name,
+                    matches: matches
+                }
+            }));
+        }
+
+        if (tournament.knockouts) {
+            allMatches.push(...tournament.knockouts.map(knockout => {
+                const matches = knockout.matches.map(match => {
+                    const playerA = tournament.players.find(player => player.id === match.playerAId);
+                    const playerB = tournament.players.find(player => player.id === match.playerBId);
+                    const winner = match.scoreA > match.scoreB 
+                        ? match.playerAId 
+                        : (match.scoreB > match.scoreA ? match.playerBId : null);
+        
+                    return {
+                        ...match,
+                        winner,
+                        playerA,
+                        playerB
+                    };
+                })
+                
+                return {
+                    name: knockout.name,
+                    matches: matches
+                }
+            }));
+        }
+        
+        return allMatches;
+    }
+
+    static async getKnockouts(id) {
+        const tournament = jsonData.find(tournament => tournament.id === id);
+
+        tournament.knockouts.map(knockout => {
+            knockout.matches.map(match => {
+                const playerA = tournament.players.find(player => player.id === match.playerAId);
+                const playerB = tournament.players.find(player => player.id === match.playerBId);
+                const winner = match.scoreA > match.scoreB 
+                    ? match.playerAId 
+                    : (match.scoreB > match.scoreA ? match.playerBId : null);
+
+                match.playerA = playerA;
+                match.playerB = playerB;
+                match.winner = winner;
+            });
+        });
+
+        return tournament.knockouts;
     }
 
     // static updateTournaments(modifiedData) {
